cea80bb533af6b84dc5d3da88bf57c921eb865b8
=Israel Montano <montano.israel@icloud.com>
2014-11-21 07:46:26 -0500

    I added a function to check if the user's input was a number

--- DrawCards/script.js
+++ DrawCards/script.js
@@ -3,3 +3,13 @@ var docObjs = {
 	btnDrawCards: document.getElementById('btnDrawCards'),
 	txtNumInput: document.getElementById('txtNumInput')
 }
+
+// this function checks to see if text input is a number
+function isNum(num) {
+	try {
+		if(isNaN(num)) throw "Your input was not a number";
+		if(num == null) throw "Your input was empty";
+	} catch(err){
+		alert(err);
+	}
+}

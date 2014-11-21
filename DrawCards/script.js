40dbdf477f57b298a41f78e48d74c82246848e1c
=Israel Montano <montano.israel@icloud.com>
2014-11-21 08:46:05 -0500

    I added a few functions but i need to finish it

--- DrawCards/script.js
+++ DrawCards/script.js
@@ -1,15 +1,61 @@
 // this is an object that stores the document objects
 var docObjs = {
 	btnDrawCards: document.getElementById('btnDrawCards'),
-	txtNumInput: document.getElementById('txtNumInput')
+	txtNumInput: document.getElementById('txtNumInput'),
+	cardsArr: new Array(9),
+	the_form: document.querySelector("#the_form")
 }
 
 // this function checks to see if text input is a number
 function isNum(num) {
 	try {
 		if(isNaN(num)) throw "Your input was not a number";
-		if(num == null) throw "Your input was empty";
+		if(num == null || num == undefined || num == "undefined") throw "Your input was empty";
 	} catch(err){
 		alert(err);
+		num = null;
+	}
+	return num;
+}
+
+// random number generator function
+function randNum(){
+	num =  Math.floor(Math.random() * 9);
+	return num;
+}
+
+// this functions generates the cards
+function drawCards(num){
+	var randomNum, times;
+	times = isNum(docObjs.txtNumInput.value);					
+		for(var i = 0; i < times ; i++){
+			randomNum = randNum();
+			// console.log(randomNum)
+			docObjs.cardsArr[i - 2] =  randomNum + 2;
+			console.log(docObjs.cardsArr[i - 2]);
+		}
+}
+
+// display draws on the label
+function showDraws(){
+	var label, text, element, 
+	num = isNum(docObjs.txtNumInput.value),
+	the_break;
+	for(var i = 0; i < num; i++){
+		label = document.createElement('label');
+		text = document.createTextNode(docObjs.cardsArr[i - 2]);
+		the_break = document.createElement('br');
+		element = docObjs.the_form;
+		label.appendChild(text);
+		element.appendChild(label);
+		element.appendChild(the_break);
 	}
 }
+
+// this is the click event
+docObjs.btnDrawCards.addEventListener('click', function(){
+	// isNum(docObjs.txtNumInput.value);
+	var num;
+	drawCards(num);
+	showDraws();
+});

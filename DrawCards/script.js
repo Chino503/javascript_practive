5767ac3f0799cbbaef2a9b9d09bcadc389e0afcb
=Israel Montano <montano.israel@icloud.com>
2014-11-21 07:43:05 -0500

    I added an object to store the document objects

---
+++ DrawCards/script.js
@@ -0,0 +1,5 @@
+// this is an object that stores the document objects
+var docObjs = {
+	btnDrawCards: document.getElementById('btnDrawCards'),
+	txtNumInput: document.getElementById('txtNumInput')
+}
